45f2d7360875f033d97cd49fc04a68f191fb408a
tintinp <tinpatana@gmail.com>
2024-06-29 23:10:01 +0700

    temp: need to fix use

--- lib/monitoring/vmstats_sink.ex
+++ lib/monitoring/vmstats_sink.ex
@@ -2,6 +2,26 @@ defmodule Monitoring.VMStatsSink do
   @behaviour :vmstats_sink
   import Telemetry.Metrics
 
+  @gauge_events [
+    "vmstats.proc_count",
+    "vmstats.proc_limit",
+    "vmstats.port_count",
+    "vmstats.port_limit",
+    "vmstats.atom_count",
+    "vmstats.messages_in_queues",
+    "vmstats.modules",
+    "vmstats.run_queue",
+    "vmstats.memory.total",
+    "vmstats.memory.procs_used",
+    "vmstats.memory.atom_used",
+    "vmstats.memory.binary",
+    "vmstats.memory.ets"
+  ]
+  @counter_events []
+  @timing_events []
+
+  use(Monitoring.VMStatsTelemetryConnector, @gauge_events, @counter_events, @timing_events)
+
   def collect(:counter, key, value) do
     # Statix.set(key, value)
   end
@@ -13,23 +33,5 @@ defmodule Monitoring.VMStatsSink do
 
   def collect(:timing, key, value) do
     # Statix.timing(key, value)
-  end
-
-  # Add new metrics here
-  def metrics() do
-    # Gauge stats
-    last_value("vmstats.proc_count.value")
-    last_value("vmstats.proc_limit.value")
-    last_value("vmstats.port_count.value")
-    last_value("vmstats.port_limit.value")
-    last_value("vmstats.atom_count.value")
-    last_value("vmstats.messages_in_queues.value")
-    last_value("vmstats.modules.value")
-    last_value("vmstats.run_queue.value")
-    last_value("vmstats.memory.total.value")
-    last_value("vmstats.memory.procs_used.value")
-    last_value("vmstats.memory.atom_used.value")
-    last_value("vmstats.memory.binary.value")
-    last_value("vmstats.memory.ets.value")
   end
 end
