3f08aa6108d309ec659c0ad605da8263ed4062e5
tintinp <tinpatana@gmail.com>
2024-06-29 17:07:31 +0700

    feat: initial commit

---
+++ lib/membrane_performance_test.ex
@@ -0,0 +1,18 @@
+defmodule MembranePerformanceTest do
+  @moduledoc """
+  Documentation for `MembranePerformanceTest`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MembranePerformanceTest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/membrane_performance_test_test.exs
@@ -0,0 +1,8 @@
+defmodule MembranePerformanceTestTest do
+  use ExUnit.Case
+  doctest MembranePerformanceTest
+
+  test "greets the world" do
+    assert MembranePerformanceTest.hello() == :world
+  end
+end
